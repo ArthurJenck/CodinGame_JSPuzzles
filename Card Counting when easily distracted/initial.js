9de260b2be768460c67aa93b6a7c7771c762cfd3
Arthur <arthurjenckdev@gmail.com>
2025-04-15 10:28:40 +0200

    feat/cardcountingeasilydistracted: added initial stat

---
+++ Card Counting when easily distracted/initial.js
@@ -0,0 +1,12 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const streamOfConsciousness = readline()
+const bustThreshold = parseInt(readline())
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('percentageChance%')

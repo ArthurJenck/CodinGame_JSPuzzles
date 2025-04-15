5e2d63505d888f45f6209500ff4712dcbf3ee646
Arthur <arthurjenckdev@gmail.com>
2025-04-15 10:25:40 +0200

    feat/mimetype: added complete solution

--- Mime Type/solution.js
+++ Mime Type/solution.js
@@ -9,8 +9,8 @@ const Q = parseInt(readline()) // Number Q of file names to be analyzed.
 const typeTable = []
 for (let i = 0; i < N; i++) {
 	var inputs = readline().split(' ')
-	const EXT = inputs[0] // file extension
-	const MT = inputs[1] // MIME type.
+	const EXT = inputs[0].toLowerCase()
+	const MT = inputs[1]
 	typeTable.push(EXT, MT)
 }
 
@@ -18,18 +18,12 @@ for (let i = 0; i < Q; i++) {
 	const FNAME = readline() // One file name per line.
 	const fileNameArray = FNAME.split('.')
 	const fileExtension =
-		fileNameArray.length > 1 ? fileNameArray[fileNameArray.length - 1] : ''
-	if (
-		(fileExtension,
-		typeTable.includes(fileExtension) ||
-			typeTable.includes(fileExtension.toLocaleLowerCase()) ||
-			typeTable.includes(fileExtension.toLocaleUpperCase()))
-	) {
-		const extensionIndex = typeTable.includes(fileExtension)
-			? typeTable.indexOf(fileExtension)
-			: typeTable.includes(fileExtension.toLocaleLowerCase())
-			? typeTable.indexOf(fileExtension.toLocaleLowerCase())
-			: typeTable.indexOf(fileExtension.toLocaleUpperCase())
+		fileNameArray.length > 1
+			? fileNameArray[fileNameArray.length - 1].toLowerCase()
+			: ''
+
+	if (typeTable.includes(fileExtension)) {
+		const extensionIndex = typeTable.indexOf(fileExtension)
 		console.log(typeTable[extensionIndex + 1])
 	} else {
 		console.log('UNKNOWN')

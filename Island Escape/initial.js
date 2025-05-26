e6854c15bb3e869fc566be290261d1feda920487
Arthur <arthurjenckdev@gmail.com>
2025-05-26 12:08:13 +0200

    feat/island escape: added initial state

---
+++ Island Escape/initial.js
@@ -0,0 +1,17 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N = parseInt(readline())
+for (let i = 0; i < N; i++) {
+	var inputs = readline().split(' ')
+	for (let j = 0; j < N; j++) {
+		const elevation = parseInt(inputs[j])
+	}
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('maybe')

1a4f2a5a91ea58660a813ac416e295fcdaa9fb4d
Arthur <arthurjenckdev@gmail.com>
2025-06-02 12:27:02 +0200

    feat/thereisnospoon: added initial state

---
+++ There is no Spoon Ep1/initial.js
@@ -0,0 +1,15 @@
+/**
+ * Don't let the machines win. You are humanity's last hope...
+ **/
+
+const width = parseInt(readline()) // the number of cells on the X axis
+const height = parseInt(readline()) // the number of cells on the Y axis
+for (let i = 0; i < height; i++) {
+	const line = readline() // width characters, each either 0 or .
+}
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+// Three coordinates: a node, its right neighbor, its bottom neighbor
+console.log('0 0 1 0 0 1')

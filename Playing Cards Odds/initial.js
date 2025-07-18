2be5f1946d9d983cf5dd8024e086dccbccf404b5
Arthur <arthurjenckdev@gmail.com>
2025-07-18 17:02:00 +0200

    added initial state

---
+++ Playing Cards Odds/initial.js
@@ -0,0 +1,19 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var inputs = readline().split(' ')
+const R = parseInt(inputs[0])
+const S = parseInt(inputs[1])
+for (let i = 0; i < R; i++) {
+  const removed = readline()
+}
+for (let i = 0; i < S; i++) {
+  const sought = readline()
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('2%')
